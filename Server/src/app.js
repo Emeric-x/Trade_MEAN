43210eff758099d0d8fb48d2fc327dd92d21b94e
Shanks lll <shanksx00@gmail.com>
2022-06-23 18:56:58 +0200

    ok

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express()
-
+const port = 3000;
 const mongoose = require('./database/mongoose')
 
-app.listen(3000, () => {
-    console.log("Server Connected on port 3000")
-})
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
+    console.log(`Server connected on port ${port}!`)
+});
