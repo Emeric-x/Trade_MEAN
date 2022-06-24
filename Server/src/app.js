96bc5e42cfaf6bbf64cd85a05f8f23e21cebc81d
Shanks lll <shanksx00@gmail.com>
2022-06-24 21:53:10 +0200

    express-graphql

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express()
 const port = 3000;
 const mongoose = require('./database/mongoose')
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.use(express.json())
 
