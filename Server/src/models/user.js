f40da2838202dc08b5a10ebaff7804fc584806e6
Shanks lll <shanksx00@gmail.com>
2022-06-29 07:13:25 +0200

    schema user updated

--- Server/src/models/user.js
+++ Server/src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = mongoose.Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
+    login: { type: String, required: true },
     password: { type: String, required: true }
 })
 
