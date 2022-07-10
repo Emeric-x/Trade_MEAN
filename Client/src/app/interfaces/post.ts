d934c3ec21e2b715f2305076a6f01513fc0a1a0f
Shanks lll <shanksx00@gmail.com>
2022-07-10 18:43:00 +0200

    post interface

--- Client/src/app/interfaces/post.ts
+++ Client/src/app/interfaces/post.ts
@@ -1,2 +1,11 @@
 export interface Post {
+    _id?: string,
+    author: {
+        author_id: string,
+        firstname: string,
+        lastname: string,
+        login: string,
+        avatar: string
+    },
+    text: string
 }
